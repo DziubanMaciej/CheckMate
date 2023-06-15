a4d58b65c7d1f31ac20e6e3a37c4ed9db47aded6
MaciejDziuban <dziuban.maciej@gmail.com>
2023-06-15 10:39:12 +0200

    Refactor executing client action into action.rs

--- client/src/action.rs
+++ client/src/action.rs
@@ -1,7 +1,131 @@
+use std::io::{Read, Write};
+
+use check_mate_common::{CommunicationError, ServerCommand};
+
 #[derive(PartialEq, Debug)]
 pub enum Action {
     ReadMessages(bool),
     WatchCommand(String, Vec<String>),
     RefreshClientByName(String),
     Abort,
+}
+
+impl Action {
+    pub fn execute<T>(
+        &self,
+        tcp_stream: &mut T,
+        client_name: &Option<String>,
+    ) -> Result<(), CommunicationError>
+    where
+        T: Read + Write,
+    {
+        if let Some(ref name) = client_name {
+            let command = ServerCommand::SetName(name.clone());
+            command.send(tcp_stream, true)?;
+        }
+
+        match self {
+            Action::ReadMessages(include_names) => Self::read(tcp_stream, *include_names),
+            Action::WatchCommand(command, command_args) => {
+                Self::watch(tcp_stream, command, command_args)
+            }
+            Action::RefreshClientByName(name) => Self::refresh_client_by_name(tcp_stream, name),
+            Action::Abort => Self::abort(tcp_stream),
+        }
+    }
+
+    fn read<T>(input_output_stream: &mut T, include_names: bool) -> Result<(), CommunicationError>
+    where
+        T: Read + Write,
+    {
+        ServerCommand::GetStatuses(include_names).send(input_output_stream, true)?;
+
+        match ServerCommand::receive_blocking(input_output_stream)? {
+            ServerCommand::Statuses(statuses) => {
+                for status in statuses.iter() {
+                    println!("{}", status);
+                }
+            }
+            _ => panic!("Unexpected command received"),
+        }
+        Ok(())
+    }
+
+    fn watch<T>(
+        output_stream: &mut T,
+        command: &str,
+        command_args: &Vec<String>,
+    ) -> Result<(), CommunicationError>
+    where
+        T: Write,
+    {
+        loop {
+            let command_output = Self::execute_command(command, command_args);
+            let command_output = command_output
+                .lines()
+                .filter(|line| !line.trim().is_empty())
+                .take(1)
+                .next()
+                .unwrap_or("")
+                .to_string();
+            let server_command = if command_output.is_empty() {
+                ServerCommand::SetStatusOk
+            } else {
+                ServerCommand::SetStatusError(command_output)
+            };
+
+            server_command.send(output_stream, true)?;
+            std::thread::sleep(std::time::Duration::from_millis(500)); // TODO make this a parameter
+        }
+    }
+
+    fn refresh_client_by_name<T>(
+        _output_stream: &mut T,
+        _name: &str,
+    ) -> Result<(), CommunicationError>
+    where
+        T: Write,
+    {
+        todo!();
+    }
+
+    fn abort<T>(output_stream: &mut T) -> Result<(), CommunicationError>
+    where
+        T: Write,
+    {
+        let command = ServerCommand::Abort;
+        command.send(output_stream, true)
+    }
+
+    fn execute_command(command: &str, command_args: &Vec<String>) -> String {
+        let subprocess = std::process::Command::new(command)
+            .args(command_args)
+            .stdout(std::process::Stdio::piped())
+            .stderr(std::process::Stdio::piped())
+            .spawn();
+        let subprocess = match subprocess {
+            Ok(x) => x,
+            Err(err) => return err.to_string(),
+        };
+
+        let subprocess_result = subprocess.wait_with_output();
+        let subprocess_result = match subprocess_result {
+            Ok(x) => x,
+            Err(err) => return err.to_string(),
+        };
+
+        let subprocess_out = if subprocess_result.status.success() {
+            subprocess_result.stdout
+        } else {
+            subprocess_result.stderr
+        };
+
+        let subprocess_out = String::from_utf8(subprocess_out);
+        let subprocess_out = match subprocess_out {
+            Ok(x) => x,
+            Err(err) => return err.to_string(),
+        };
+
+        subprocess_out
+    }
 }
--- client/src/main.rs
+++ client/src/main.rs
@@ -1,9 +1,7 @@
-use check_mate_common::{CommunicationError, ServerCommand};
 use std::net::{Ipv4Addr, SocketAddrV4, TcpStream};
 mod action;
 mod config;
 
-use action::Action;
 use config::Config;
 
 fn connect_to_server(server_address: SocketAddrV4) -> TcpStream {
@@ -11,7 +9,7 @@ fn connect_to_server(server_address: SocketAddrV4) -> TcpStream {
         let tcp_stream = match TcpStream::connect(server_address) {
             Ok(ok) => ok,
             Err(err) => {
-                println!("Failed to connect with server: {}. Keep waiting.", err);
+                eprintln!("Failed to connect with server: {}. Keep waiting.", err);
                 std::thread::sleep(std::time::Duration::from_millis(500)); // TODO make this a parameter
                 continue;
             }
@@ -20,125 +18,21 @@ fn connect_to_server(server_address: SocketAddrV4) -> TcpStream {
     }
 }
 
-fn execute_action(config: &Config, tcp_stream: &mut TcpStream) -> Result<(), CommunicationError> {
-    if let Some(ref name) = config.client_name {
-        let command = ServerCommand::SetName(name.clone());
-        command.send(tcp_stream, true)?;
-    }
-
-    match &config.action {
-        Action::ReadMessages(include_names) => {
-            read_messages_from_server(tcp_stream, *include_names)
-        }
-        Action::WatchCommand(command, command_args) => {
-            watch_command(tcp_stream, command, command_args)
-        }
-        Action::RefreshClientByName(name) => refresh_client_by_name(tcp_stream, name),
-        Action::Abort => abort_server(tcp_stream),
-    }
-}
-
-fn read_messages_from_server(
-    tcp_stream: &mut TcpStream,
-    include_names: bool,
-) -> Result<(), CommunicationError> {
-    ServerCommand::GetStatuses(include_names).send(tcp_stream, true)?;
-
-    match ServerCommand::receive_blocking(tcp_stream)? {
-        ServerCommand::Statuses(statuses) => {
-            for status in statuses.iter() {
-                println!("{}", status);
-            }
-        }
-        _ => panic!("Unexpected command received"),
-    }
-    Ok(())
-}
-
-fn execute_command(command: &str, command_args: &Vec<String>) -> String {
-    let subprocess = std::process::Command::new(command)
-        .args(command_args)
-        .stdout(std::process::Stdio::piped())
-        .stderr(std::process::Stdio::piped())
-        .spawn();
-    let subprocess = match subprocess {
-        Ok(x) => x,
-        Err(err) => return err.to_string(),
-    };
-
-    let subprocess_result = subprocess.wait_with_output();
-    let subprocess_result = match subprocess_result {
-        Ok(x) => x,
-        Err(err) => return err.to_string(),
-    };
-
-    let subprocess_out = if subprocess_result.status.success() {
-        subprocess_result.stdout
-    } else {
-        subprocess_result.stderr
-    };
-
-    let subprocess_out = String::from_utf8(subprocess_out);
-    let subprocess_out = match subprocess_out {
-        Ok(x) => x,
-        Err(err) => return err.to_string(),
-    };
-
-    subprocess_out
-}
-
-fn watch_command(
-    tcp_stream: &mut TcpStream,
-    command: &str,
-    command_args: &Vec<String>,
-) -> Result<(), CommunicationError> {
-    loop {
-        let command_output = execute_command(command, command_args);
-        let command_output = command_output
-            .lines()
-            .filter(|line| !line.trim().is_empty())
-            .take(1)
-            .next()
-            .unwrap_or("")
-            .to_string();
-        let server_command = if command_output.is_empty() {
-            ServerCommand::SetStatusOk
-        } else {
-            ServerCommand::SetStatusError(command_output)
-        };
-
-        server_command.send(tcp_stream, true)?;
-        std::thread::sleep(std::time::Duration::from_millis(500)); // TODO make this a parameter
-    }
-}
-
-fn refresh_client_by_name(
-    _tcp_stream: &mut TcpStream,
-    _name: &str,
-) -> Result<(), CommunicationError> {
-    todo!();
-}
-
-fn abort_server(tcp_stream: &mut TcpStream) -> Result<(), CommunicationError> {
-    let command = ServerCommand::Abort;
-    command.send(tcp_stream, true)
-}
-
 fn main() {
     let config = Config::parse(std::env::args().skip(1));
     let config = match config {
         Ok(x) => x,
         Err(err) => {
-            println!("ERROR: {}", err);
+            eprintln!("ERROR: {}", err);
             std::process::exit(1);
         }
     };
 
     let server_address = SocketAddrV4::new(Ipv4Addr::LOCALHOST, config.server_port);
     let mut tcp_stream = connect_to_server(server_address);
-    let action_result = execute_action(&config, &mut tcp_stream);
+    let action_result = config.action.execute(&mut tcp_stream, &config.client_name);
     if let Err(err) = action_result {
-        println!("ERROR: {}", err);
+        eprintln!("ERROR: {}", err);
         std::process::exit(1);
     }
 }
